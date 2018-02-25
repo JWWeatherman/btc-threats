b1ca8a135fd790ad79b6681c90f5df7504c870e3
Richard Portsmith <gifted.primate@protonmail.com>
2018-02-25 15:34:03 -0500

    init

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require('express')
+var serveStatic = require('serve-static')
+
+var app = express()
+var port = process.env.PORT || 5000
+var host = process.env.HOST || 'localhost'
+ 
+app.use(serveStatic(__dirname))
+
+app.listen(3000,  host, function () {
+  console.log('App running at http://' + host + ':' + port)
+})
